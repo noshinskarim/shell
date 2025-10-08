a399ba98b5dfd212fd041bd68ff0d1bfc27a78f7
Noshin <noshin.karim@mail.utoronto.ca>
2025-10-07 22:53:43 -0400

    test 1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,15 +28,27 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv "./rawdata" "./data/raw"
 # 3. List the contents of the ./data/raw directory
-
+cd data
+ls 
+pwd
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir processed 
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cd "../../"
+pwd
+cd "./data/raw"
+for file in ./*; do
+  if [[ "$file" == *server* && "$file" == *.log ]]; then
+    cp "$file" "../processed/server_logs"
+  fi
+done
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
